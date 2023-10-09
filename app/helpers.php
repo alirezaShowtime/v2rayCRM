4517ea3deb41157e6289f4573a01119262366c4a
Alireza <karayyb@gmail.com>
2023-10-09 14:24:59 +0330

    a helpers.php file is created

---
+++ app/helpers.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Utils\ResponseUtil;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+function successRes(
+    array  $body = null,
+    bool   $status = true,
+    string $message = "",
+)
+{
+    return ResponseUtil::successRes($body, $status, $message);
+}
+
+function successJsonResource(
+    JsonResource $body,
+    Request      $request,
+    bool         $status = true,
+    string       $message = "",
+)
+{
+    return ResponseUtil::successJsonResource($body, $request, $status, $message);
+}
+
+function errorRes(
+    int    $code,
+    string $message,
+    array  $error = null,
+    int    $errorCode = null,
+)
+{
+    return ResponseUtil::errorRes($code, $message, $error, $errorCode);
+}
+
+function error500Res()
+{
+    return ResponseUtil::error500Res();
+}
