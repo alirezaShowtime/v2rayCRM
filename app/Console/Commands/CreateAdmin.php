c9feb57a261fa1ff3fa6e3399f324436dce002e1
Alireza <karayyb@gmail.com>
2023-10-09 13:59:58 +0330

    createAdmin command line is created

---
+++ app/Console/Commands/CreateAdmin.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Admin;
+use Illuminate\Console\Command;
+
+class CreateAdmin extends Command
+{
+    protected $signature = 'app:create-admin';
+
+    protected $description = 'Creating admin with CLI';
+
+    public function handle()
+    {
+        $username = $this->ask('enter admin username');
+        $password = $this->ask('enter admin password');
+
+        $this->info("username: $username");
+        $this->info("password: $password");
+
+        if (!$this->confirm("Are you sure create admin with this data? ")) {
+
+            $this->info('creating admin is canceled');
+            return;
+        }
+
+        $admin = Admin::create([
+            "username" => $username,
+            "password" => $password,
+        ]);
+
+        $this->info($admin->makeVisible("password")->toJson(JSON_PRETTY_PRINT));
+
+        $this->info("\nAdmin is created :)");
+
+    }
+}
