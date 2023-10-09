21b02f81295d52bd9250b1828ce5655cd0ad6709
Alireza <karayyb@gmail.com>
2023-10-09 15:10:10 +0330

    a login api is created

---
+++ app/Http/Controllers/Admin/AuthController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Admin;
+use App\Utils\JWTUtil;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $request->validate([
+            "username" => "required",
+            "password" => "required",
+        ], [
+            "username" => "نام کاربری",
+            "password" => "رمز عبور",
+        ]);
+
+        $admin = Admin::where("username", $request->username)->first();
+
+        if ($admin == null || Hash::check($admin->password, $request->password)) {
+            return errorRes(404, "نام کاربری یا رمز عبور اشتباه است.");
+        }
+
+        return successRes([
+            "token" => [
+                "access" => JWTUtil::generateForAdmin($admin),
+                "refresh" => JWTUtil::generateForAdmin($admin, JWTUtil::getRefreshTokenLifetime()),
+            ],
+        ]);
+
+    }
+
+}
