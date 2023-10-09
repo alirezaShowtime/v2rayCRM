03b76267e99f58e203a45fa173ba606198ae8f22
Alireza <karayyb@gmail.com>
2023-10-09 23:14:17 +0330

    The getConfig api of user is created

---
+++ app/Http/Controllers/Admin/V2rayConfigController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Admin\V2rayConfigCreateRequest;
+use App\Http\Resources\V2rayConfigResource;
+use App\Models\User;
+use App\Models\V2rayConfig;
+use Illuminate\Http\Request;
+
+class V2rayConfigController extends Controller
+{
+
+    public function create(V2rayConfigCreateRequest $request, int $id)
+    {
+        if (User::find($id) == null) {
+
+            return errorRes(404, "کاربری با این شناسه پیدا نشد.");
+        }
+
+        try {
+            $config = V2rayConfig::create([
+                'remark' => $request->remark,
+                'size' => $request->size,
+                'days' => $request->days,
+                'price' => $request->price,
+                'user_id' => $id,
+                'admin_id' => request()->admin->id,
+            ]);
+
+            return successJsonResource(new V2rayConfigResource($config), $request);
+
+        } catch (\Exception $e) {
+            return error500Res();
+
+        }
+
+    }
+
+    public function getAll(Request $request, int $id)
+    {
+
+        if (User::find($id) == null) {
+            return errorRes(404, "کاربری با این شناسه یافت نشد.");
+        }
+
+        $q = $request->query("q", "");
+        $sort = $request->query("sort", "desc");
+        $filter = $request->query("filter", 'all');
+
+        if (!in_array($sort, ["desc", "asc"])) {
+            return errorRes(400, "the sort query param must be 'desc' or 'acs'");
+        }
+
+        if (!in_array($filter, ["all", "disabled", 'enabled'])) {
+            return errorRes(400, "the filter query param must be 'all' or 'disabled' or 'enabled'");
+        }
+
+
+        $configQuery = V2rayConfig::query()->where('user_id', $id)->orderBy('id', $sort);
+
+        if (!empty($q)) {
+            $configQuery->whereRaw("LOWER(`remark`) like \"%$q%\"");
+        }
+
+        switch ($filter) {
+            case "all" :
+                break;
+            case "disabled":
+                $configQuery->where('enabled_at', null);
+                break;
+            case "enabled":
+                $configQuery->whereNot('enabled_at', null);
+                break;
+        }
+
+        try {
+
+            $configs = $configQuery->get();
+
+        } catch (\Exception $e) {
+            return error500Res();
+        }
+        return successJsonResource(V2rayConfigResource::collection($configs), $request);
+    }
+
+}
--- app/Http/Controllers/V2rayConfigController.php
+++ app/Http/Controllers/V2rayConfigController.php
@@ -2,86 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use App\Http\Requests\Admin\V2rayConfigCreateRequest;
+use App\Exceptions\MarzbanException;
 use App\Http\Resources\V2rayConfigResource;
-use App\Models\User;
 use App\Models\V2rayConfig;
+use App\Utils\MarzbanUtil;
 use Illuminate\Http\Request;
 
 class V2rayConfigController extends Controller
 {
-
-    public function create(V2rayConfigCreateRequest $request, int $id)
+    public function get(Request $request, int $id)
     {
-        if (User::find($id) == null) {
+        $config = V2rayConfig::find($id);
 
-            return errorRes(404, "کاربری با این شناسه پیدا نشد.");
-        }
-
-        try {
-            $config = V2rayConfig::create([
-                'remark' => $request->remark,
-                'size' => $request->size,
-                'days' => $request->days,
-                'price' => $request->price,
-                'user_id' => $id,
-                'admin_id' => request()->admin->id,
-            ]);
-
-            return successJsonResource(new V2rayConfigResource($config), $request);
-
-        } catch (\Exception $e) {
-            return error500Res();
-
-        }
-
-    }
-
-    public function getAll(Request $request, int $id)
-    {
-
-        if (User::find($id) == null) {
-            return errorRes(404, "کاربری با این شناسه یافت نشد.");
-        }
-
-        $q = $request->query("q", "");
-        $sort = $request->query("sort", "desc");
-        $filter = $request->query("filter", 'all');
-
-        if (!in_array($sort, ["desc", "asc"])) {
-            return errorRes(400, "the sort query param must be 'desc' or 'acs'");
-        }
-
-        if (!in_array($filter, ["all", "disabled", 'enabled'])) {
-            return errorRes(400, "the filter query param must be 'all' or 'disabled' or 'enabled'");
-        }
-
-
-        $configQuery = V2rayConfig::query()->where('user_id', $id)->orderBy('id', $sort);
-
-        if (!empty($q)) {
-            $configQuery->whereRaw("LOWER(`remark`) like \"%$q%\"");
-        }
-
-        switch ($filter) {
-            case "all" :
-                break;
-            case "disabled":
-                $configQuery->where('enabled_at', null);
-                break;
-            case "enabled":
-                $configQuery->whereNot('enabled_at', null);
-                break;
+        if ($config == null) {
+            return errorRes(404, "کانفیگی با این شناسه یافت نشد.");
         }
 
         try {
 
-            $configs = $configQuery->get();
+            $config = MarzbanUtil::getConfig($config);
 
-        } catch (\Exception $e) {
+        } catch (MarzbanException $e) {
+
             return error500Res();
         }
-        return successJsonResource(V2rayConfigResource::collection($configs), $request);
+
+        return successJsonResource(new V2rayConfigResource($config), $request);
+
     }
 
 }
