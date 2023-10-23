f18afd523ddd41ad4e96575119a12d54fb00fbb6
Akbari <karayyb@gmail.com>
2023-10-23 18:18:48 +0330

    V2rayConfigResource updated

--- app/Http/Resources/V2rayConfigResource.php
+++ app/Http/Resources/V2rayConfigResource.php
@@ -4,19 +4,25 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Carbon;
 
 class V2rayConfigResource extends JsonResource
 {
 
     public function toArray(Request $request): array
     {
+
+        
         return [
             'id' => $this->id,
             'remark' => $this->remark,
             'size' => $this->size,
             'price' => $this->price,
-            'enabled_at' => $this->enabled_at,
-            'expired_at' => $this->expired_at,
+            'owner' => $this->user->username,
+            'enabled_at' => Carbon::parse($this->enabled_at)->timestamp,
+            'expired_at' => Carbon::parse($this->expired_at)->timestamp,
+            'created_at' => Carbon::parse($this->created_ath)->timestamp,
+            'days' => $this->days,
             'config_data' => $this->config_data,
         ];
     }
